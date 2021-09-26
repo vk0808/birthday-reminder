bb94c2a99e9f09098f3e6bc8282bcc7b64c32883
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-09-26 21:20:45 +0530

    modify style, add label to btns

--- src/List.js
+++ src/List.js
@@ -14,38 +14,41 @@ const List = ({ people, removePerson, editPerson, today, setAmount }) => {
   });
   setAmount(filteredData.length);
   const birthday = today === true ? filteredData : people;
-  console.log(filteredData);
   return (
     <>
-      {birthday.map((person) => {
-        const { id, name, age, date, image } = person;
-        return (
-          <article key={id} className="person">
-            <img src={image} alt={name} />
-            <div className="pesron-info">
-              <h4>{name}</h4>
-              <p>{moment(date).format("MMM Do YYYY")}</p>
-              <p>{age}</p>
-              <Birthday
-                name={name}
-                age={age}
-                day={moment(date).format("D")}
-                month={moment(date).format("M")}
-              />
-            </div>
-            <div className="btns">
-              <button className="btn" onClick={() => editPerson(id)}>
-                <a href="#edit">
-                  <FaEdit size="20" />
-                </a>
-              </button>
-              <button className="btn">
-                <FaTrash size="20" onClick={() => removePerson(id)} />
-              </button>
-            </div>
-          </article>
-        );
-      })}
+      {today === true && birthday.length === 0 ? (
+        <div className="no-birthday-today">No birthdays today</div>
+      ) : (
+        birthday.map((person) => {
+          const { id, name, age, date, image } = person;
+          return (
+            <article key={id} className="person">
+              <img src={image} alt={name} />
+              <div className="pesron-info">
+                <h4>{name}</h4>
+                <p>{moment(date).format("MMM Do YYYY")}</p>
+                <p>{age}</p>
+                <Birthday
+                  name={name}
+                  age={age}
+                  day={moment(date).format("D")}
+                  month={moment(date).format("M")}
+                />
+              </div>
+              <div className="btns" aria-label="Edit">
+                <button className="btn" onClick={() => editPerson(id)}>
+                  <a href="#edit">
+                    <FaEdit size="20" />
+                  </a>
+                </button>
+                <button className="btn" aria-label="Delete">
+                  <FaTrash size="20" onClick={() => removePerson(id)} />
+                </button>
+              </div>
+            </article>
+          );
+        })
+      )}
     </>
   );
 };
